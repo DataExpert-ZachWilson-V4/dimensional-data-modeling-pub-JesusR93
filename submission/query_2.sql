879575b59d387667fe3fcb833bea3c6583fb1a82
Jesus Rivera <jesusr.contacto@gmail.com>
2024-05-21 08:41:16 -0600

    Formatted

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,41 +1,60 @@
 INSERT INTO
-  jesusr93.actors
+jesusr93.actors
 WITH last_year AS (
-    SELECT actor, actor_id, films, quality_class, is_active, current_year
+    SELECT
+        actor,
+        actor_id,
+        films,
+        quality_class,
+        is_active,
+        current_year
     FROM jesusr93.actors
-    WHERE current_year= 1923
-  ),
+    WHERE current_year = 1923
+),
 
 this_year AS (
     -- I decided to group as one actor can have more than one film in a year
     -- Added the year to the array will let us to explode it preserving the year
-    SELECT actor, actor_id, year,
+    SELECT
+        actor,
+        actor_id,
+        year,
         ARRAY_AGG(
-            CAST(ROW(film, votes, rating, film_id, year) AS ROW(film VARCHAR, votes BIGINT, rating DOUBLE, film_id VARCHAR, year INTEGER))
+            CAST(
+                ROW(film, votes, rating, film_id, year)
+                AS ROW (
+                    film VARCHAR,
+                    votes BIGINT,
+                    rating DOUBLE,
+                    film_id VARCHAR,
+                    year INTEGER
+                )
+            )
             ORDER BY film_id
         ) AS films,
         AVG(rating) AS avg_rating
     FROM bootcamp.actor_films
     WHERE year = 1924
     GROUP BY actor, actor_id, year
-  )
+)
+
 SELECT
     COALESCE(ly.actor, ty.actor) AS actor,
     COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
     CASE
         WHEN ty.films IS NULL THEN ly.films
         WHEN ty.films IS NOT NULL AND ly.films IS NULL THEN ty.films
-        WHEN ty.films IS NOT NULL AND ly.films IS NOT NULL THEN ty.films || ly.films
+        WHEN ty.films IS NOT NULL AND ly.films IS NOT NULL
+            THEN ty.films || ly.films
     END AS films,
-    CASE WHEN ty.avg_rating IS NULL THEN ly.quality_class
-    ELSE
-        CASE WHEN avg_rating > 8 THEN 'star'
-            WHEN avg_rating > 7 AND avg_rating <= 8 THEN 'good'
-            WHEN avg_rating > 6 AND avg_rating <= 7 THEN 'average'
-            WHEN avg_rating <= 6 THEN 'bad'
-        END 
+    CASE
+        WHEN ty.avg_rating IS NULL THEN ly.quality_class
+        WHEN ty.avg_rating > 8 THEN 'star'
+        WHEN ty.avg_rating > 7 AND ty.avg_rating <= 8 THEN 'good'
+        WHEN ty.avg_rating > 6 AND ty.avg_rating <= 7 THEN 'average'
+        WHEN ty.avg_rating <= 6 THEN 'bad'
     END AS quality_class,
     ty.year IS NOT NULL AS is_active,
     COALESCE(ty.year, ly.current_year + 1) AS current_year
-FROM last_year ly
-FULL OUTER JOIN this_year ty ON ly.actor_id = ty.actor_id
+FROM last_year AS ly
+FULL OUTER JOIN this_year AS ty ON ly.actor_id = ty.actor_id
--- submission/query_5.sql
+++ submission/query_5.sql
@@ -3,85 +3,126 @@ WITH
 last_year_scd AS (
     SELECT *
     FROM jesusr93.actors_history_scd
-    WHERE current_year = 1924),
+    WHERE current_year = 1924
+),
 
 current_year_scd AS (
     SELECT *
     FROM jesusr93.actors
-    WHERE current_year = 1925),
+    WHERE current_year = 1925
+),
 
 combined AS (
-  -- combined years
-SELECT
-  COALESCE(ly.actor_id, cy.actor_id) AS actor_id,
-  COALESCE(ly.start_year, cy.current_year) AS start_year,
-  COALESCE(ly.end_year, cy.current_year) AS end_year,
-  -- did active change?
-  CASE WHEN  COALESCE(ly.is_active, FALSE) <> cy.is_active THEN 1
-    WHEN COALESCE(ly.is_active,FALSE) = cy.is_active THEN 0
-  END AS active_change,
-  -- active for this and last year 
-  COALESCE(ly.is_active,FALSE) AS is_active_last_year,
-  cy.is_active AS is_active_this_year,
-  -- did quality_class changed?
-  CASE WHEN COALESCE(ly.quality_class,'') <> cy.quality_class THEN 1
-    WHEN COALESCE(ly.quality_class,'') = cy.quality_class THEN 0
-  END AS qc_change,
-  -- quality class values for this and last year
-  COALESCE(ly.quality_class,'') AS qc_last_year,
-  cy.quality_class AS qc_this_year,
-  -- current year
-  1925 AS current_year
-FROM last_year_scd ly
-FULL OUTER JOIN current_year_scd cy
-  ON ly.actor_id = cy.actor_id
-  AND ly.end_year + 1 = cy.current_year
+    -- combined years
+    SELECT
+        COALESCE(ly.actor_id, cy.actor_id) AS actor_id,
+        COALESCE(ly.start_year, cy.current_year) AS start_year,
+        COALESCE(ly.end_year, cy.current_year) AS end_year,
+        -- did active change?
+        CASE
+            WHEN COALESCE(ly.is_active, FALSE) <> cy.is_active THEN 1
+            WHEN COALESCE(ly.is_active, FALSE) = cy.is_active THEN 0
+        END AS active_change,
+        -- active for this and last year 
+        COALESCE(ly.is_active, FALSE) AS is_active_last_year,
+        cy.is_active AS is_active_this_year,
+        -- did quality_class changed?
+        CASE
+            WHEN COALESCE(ly.quality_class, '') <> cy.quality_class THEN 1
+            WHEN COALESCE(ly.quality_class, '') = cy.quality_class THEN 0
+        END AS qc_change,
+        -- quality class values for this and last year
+        COALESCE(ly.quality_class, '') AS qc_last_year,
+        cy.quality_class AS qc_this_year,
+        -- current year
+        1925 AS current_year
+    FROM last_year_scd AS ly
+    FULL OUTER JOIN current_year_scd AS cy ON
+        ly.actor_id = cy.actor_id
+        AND ly.end_year + 1 = cy.current_year
 ),
+
 changes AS (
-  SELECT
-    actor_id,
-    current_year,
-    CASE
-      -- If both dimensions remain equal
-      WHEN active_change = 0 AND qc_change = 0
-        THEN ARRAY[
-          CAST(
-            ROW(is_active_last_year, qc_last_year, start_year, end_year + 1)
-            AS ROW(is_active boolean, qc VARCHAR, start_year integer, end_year integer)
-          )]
-      -- If some of the dimensions changed
-      WHEN active_change = 1 OR qc_change = 1
-        THEN ARRAY[
-          CAST(
-            ROW(is_active_last_year, qc_last_year, start_year, end_year)
-            AS ROW(is_active boolean, qc VARCHAR, start_year integer, end_year integer)
-          ),
-          CAST(
-            ROW(is_active_this_year, qc_this_year, current_year, current_year)
-            AS ROW(is_active boolean, qc VARCHAR, start_year integer, end_year integer)
-          )
-        ]
-        WHEN active_change IS NULL OR qc_change IS NULL THEN ARRAY[
-          CAST(
-            ROW(
-              COALESCE(is_active_last_year, is_active_this_year),
-              COALESCE(qc_last_year, qc_this_year),
-              start_year,
-              end_year)
-            AS ROW(is_active boolean, qc VARCHAR, start_year integer, end_year integer)
-          )
-        ]
-      END AS change_array
-    FROM
-      combined
-  )
+    SELECT
+        actor_id,
+        current_year,
+        CASE
+            -- If both dimensions remain equal
+            WHEN active_change = 0 AND qc_change = 0
+                THEN ARRAY[
+                    CAST(
+                        ROW(
+                            is_active_last_year,
+                            qc_last_year,
+                            start_year,
+                            end_year + 1
+                        )
+                        AS ROW (
+                            is_active boolean,
+                            qc VARCHAR,
+                            start_year integer,
+                            end_year integer
+                        )
+                    )
+                ]
+            -- If some of the dimensions changed
+            WHEN active_change = 1 OR qc_change = 1
+                THEN ARRAY[
+                    CAST(
+                        ROW(
+                            is_active_last_year,
+                            qc_last_year,
+                            start_year,
+                            end_year
+                        )
+                        AS ROW (
+                            is_active boolean,
+                            qc VARCHAR,
+                            start_year integer,
+                            end_year integer
+                        )
+                    ),
+                    CAST(
+                        ROW(
+                            is_active_this_year,
+                            qc_this_year,
+                            current_year,
+                            current_year
+                        )
+                        AS ROW (
+                            is_active boolean,
+                            qc VARCHAR,
+                            start_year integer,
+                            end_year integer
+                        )
+                    )
+                ]
+            WHEN active_change IS NULL OR qc_change IS NULL THEN ARRAY[
+                CAST(
+                    ROW(
+                        COALESCE(is_active_last_year, is_active_this_year),
+                        COALESCE(qc_last_year, qc_this_year),
+                        start_year,
+                        end_year
+                    )
+                    AS ROW (
+                        is_active boolean,
+                        qc VARCHAR,
+                        start_year integer,
+                        end_year integer
+                    )
+                )
+            ]
+        END AS change_array
+    FROM combined
+)
+
 SELECT
-  actor_id,
-  arr.qc,
-  arr.is_active,
-  arr.start_year,
-  arr.end_year,
-  current_year
-FROM
-  changes
-  CROSS JOIN UNNEST (change_array) AS arr
+    changes.actor_id,
+    arr.qc,
+    arr.is_active,
+    arr.start_year,
+    arr.end_year,
+    changes.current_year
+FROM changes
+CROSS JOIN UNNEST(changes.change_array) AS arr
